f6910ea6160a0d3b987f7d8ccc49e6f94f25d662
Akshay Patil <140628706+akshay7485@users.noreply.github.com>
2024-12-23 19:50:42 +0530

    Update test_portfolio.py

--- test_portfolio.py
+++ test_portfolio.py
@@ -24,7 +24,7 @@ def test_portfolio_homepage():
 
     # Check if the word "Portfolio" is in the title of the page
 
-    assert "Portfolio" in driver.title
+    assert "Onix Digital Marketing HTML5 Template" in driver.title
 
     
 
